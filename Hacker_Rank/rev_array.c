f73c07425051efc4e31b36df441e17b06ec6dff8
Sharan Rathnakumar <53375728+sharanrathnakumar@users.noreply.github.com>
2023-04-06 23:35:26 +0530

    Create rev_array.c

---
+++ Hacker_Rank/rev_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+
+
+    /* Write the logic to reverse the array. */
+    
+    int temp, j = num-1;
+    for(i = 0; i <num/2 ; i++)
+    {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j--]= temp;
+    }
+    
+    for(i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+    return 0;
+}
